864c92724880f7348c2dd154194446bd6b1955cc
lynnyeah <lynnjeancaraballo@gmail.com>
2025-07-24 08:03:58 -0700

    Update Database.php

--- src/Core/Database.php
+++ src/Core/Database.php
@@ -13,7 +13,7 @@ class Database {
         $user = 'root';
         $pass = '';
         //mysqli connection
-        $this->conn new \mysqli($host, $user, $pass, $db);
+        $this->conn = new mysqli($host, $user, $pass, $db);
         if ($this->conn->connect_error) {
             die("Connection failed: " . $this->conn->connect_error);
         }
